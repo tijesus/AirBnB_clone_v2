cec269b88fd8d020caa71ab81aaa1b3a4bd2abd4
tijesus <emmanueltijesu@gmail.com>
2024-05-06 13:49:15 +0100

    remaining task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-from fabric import task
-import sys
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
 
-@task
-def my_task(c):
-    c.run("ls -l")
+from datetime import datetime
+from fabric.api import *
 
-if __name__ == "__main__":
-    my_task(sys.argv[1])
+
+def do_pack():
+    """
+    making an archive on web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
